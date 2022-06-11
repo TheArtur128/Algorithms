fe8948507a7e95ca1630d7d50b44aa4d71a27002
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-06-11 14:05:23 +0700

    Implementation of binary trees

--- structures/graphs.py
+++ structures/graphs.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Callable
+from typing import Iterable, Callable, Union
 from abc import ABC, abstractmethod
 
 from errors import NoGraphNodeReference, NoNextGraphNode
@@ -167,6 +167,130 @@ class NumericBinaryGraphNode(SystematizedBinaryGraphNode):
     @property
     def determinant_function(self) -> Callable:
         return lambda new_data, old_data: 'left' if new_data < old_data else 'right'
+
+
+class IBinaryTree(ABC):
+    """Describes the behavior of a binary tree."""
+
+    def __repr__(self):
+        return "{class_name}(top_data={top_data}, data_amount={data_amount})".format(
+            class_name=self.__class__.__name__,
+            top_data=self.top_data,
+            data_amount=self.data_amount
+        )
+
+    @property
+    @abstractmethod
+    def data_amount(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def top_data(self) -> any:
+        pass
+
+    @abstractmethod
+    def __getitem__(self, data_key: any) -> tuple:
+        pass
+
+    def add_from(self, items: Iterable) -> None:
+        for item in items:
+            self.add(item)
+
+    @abstractmethod
+    def add(self, data: any) -> None:
+        pass
+
+    @abstractmethod
+    def cut(self, data: any) -> None:
+        pass
+
+
+class BinaryTree(IBinaryTree):
+    """
+    Binary tree using binary nodes, the class of which must be given during
+    initialization.
+    """
+
+    def __init__(self, node_type: SystematizedBinaryGraphNode, items: Iterable = tuple()):
+        self.__node_type = node_type
+        self.__top_node = self.__node_type(None)
+        self.add_from(items)
+
+    @property
+    def data_amount(self) -> int:
+        return sum((
+            len(self.__recursive_get_all_nodes_from(self.__top_node)),
+            -1 if self.__top_node.data is None else 0
+        ))
+
+    @property
+    def top_data(self) -> any:
+        return self.__top_node.data
+
+    def __getitem__(self, node_data: any) -> tuple:
+        found_node = self.__recursive_found_nodes_to_node_with_data(node_data)[-1]
+
+        if found_node is None:
+            raise KeyError(node_data)
+
+        return tuple(map(lambda node: node.data, found_node.nodes))
+
+    def add_from(self, items: Iterable) -> None:
+        for item in items:
+            self.add(item)
+
+    def add(self, data: any) -> None:
+        if self.__top_node.data is None:
+            self.__top_node.data = data
+        else:
+            self.__top_node.add_node(self.__node_type(data))
+
+    def cut(self, data: any) -> None:
+        previous_node, desired_node = self.__recursive_found_nodes_to_node_with_data(
+            data,
+            self.__top_node
+        )[-2:]
+
+        if desired_node is None:
+            raise ValueError("{self} doesn't have node with data {data}")
+        elif previous_node is None:
+            self.__top_node.data = None
+            map(self.__top_node.cut_node, self.__top_node.nodes)
+        else:
+            previous_node.cut_node(desired_node)
+
+    def __recursive_found_nodes_to_node_with_data(
+        self,
+        data: any,
+        active_node: AbstractBinaryGraphNode | None,
+        previous_nodes: tuple[AbstractBinaryGraphNode,] = tuple(),
+    ) -> tuple[Union[AbstractBinaryGraphNode, None],]:
+        if active_node is None or active_node.data == data:
+            return *previous_nodes, active_node
+
+        match active_node.determinant_function(data, active_node.data):
+            case "right":
+                select_next_node = active_node.right_node
+            case "left":
+                select_next_node = active_node.left_node
+
+        return self.__recursive_found_nodes_to_node_with_data(
+            data,
+            select_next_node,
+            (*previous_nodes, active_node)
+        )
+
+    def __recursive_get_all_nodes_from(
+        self,
+        node: AbstractBinaryGraphNode
+    ) -> list[AbstractBinaryGraphNode,]:
+        nodes = [node]
+
+        for bottom_node in node.nodes:
+            nodes.extend(self.__recursive_get_all_nodes_from(bottom_node))
+
+        return nodes
 
 
 class GraphPath:
